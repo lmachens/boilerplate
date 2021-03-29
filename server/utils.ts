a2b06eb942b7e41213d62543eb43994f41e12c50
lmachens <leon@machens.koeln>
2021-03-29 14:52:43 +0200

    🚨 Return handler API resolved without sending a response for /api/students, this may result in stalled requests.

--- server/utils.ts
+++ server/utils.ts
@@ -6,6 +6,6 @@ type Handler = (req: NextApiRequest, res: NextApiResponse) => void;
 export function withDatabase(handler: Handler) {
   return async function (req: NextApiRequest, res: NextApiResponse) {
     await connect(process.env.MONGODB_URI);
-    handler(req, res);
+    return handler(req, res);
   };
 }
